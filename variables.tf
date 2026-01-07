d7a4c6ba8f1500060f70bbd19cc48f9b35a78b5a
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-07 10:34:57 +0530

    terraform-vpc-module

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "project_name" {
-  
+
 }
 
 variable "environment" {
-  
+
 }
 
 variable "vpc_cidr" {
-  
+
 }
 
 variable "enable_dns_hostnames" {
@@ -15,10 +15,66 @@ variable "enable_dns_hostnames" {
 }
 
 variable "common_tags" {
-  type = map 
+  type = map(any)
   #default = {}
 }
 
 variable "vpc_tags" {
+  default = {}
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subent_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.public_subent_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet cidrs"
+  }
+}
+
+variable "public_subnet_tags" {
+  default = {}
+}
+
+variable "private_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet cidrs"
+  }
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database subnet cidrs"
+  }
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
+
+variable "nat_gateway_tags" {
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
   default = {}
 }
